3d184514371d543fdc33cc3f25c94a33c8824836
Hyakusetufutou <Hyakusetufutou358@gmail.com>
2025-01-18 16:07:01 +0900

    Initial commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//  
+//  
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
